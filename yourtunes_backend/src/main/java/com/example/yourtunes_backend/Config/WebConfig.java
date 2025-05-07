dddb4d6b458e58ae94f5bfbad37aa06205c6b0e1
ManaP0tion <aer0dev7@gmail.com>
2025-05-07 11:46:52 +0900

    [CREATE] Add Proxy to Project

---
+++ yourtunes_backend/src/main/java/com/example/yourtunes_backend/Config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.yourtunes_backend.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")  // React 주소
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
