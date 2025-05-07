2922068613cc5a51b0434c58de882b2e64ce7ddc
ManaP0tion <aer0dev7@gmail.com>
2025-05-07 11:09:47 +0900

    init

---
+++ yourtunes_backend/src/main/java/com/example/yourtunes/YourtunesApplication.java
@@ -0,0 +1,13 @@
+package com.example.yourtunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourtunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourtunesApplication.class, args);
+	}
+
+}
